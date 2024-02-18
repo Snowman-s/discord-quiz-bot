382e98a11f8c06cd2ed92979fc2ad2b29f23ba26
Snowman-s <snowman.snowsnow@gmail.com>
2024-02-18 17:35:49 +0900

    エラー処理を追加

--- src/ygo.rs
+++ src/ygo.rs
@@ -48,45 +48,72 @@ pub(crate) fn create_command(c: CreateCommand) -> CreateCommand {
 }
 
 pub(crate) async fn receive_command(bot: &Bot, ctx: &Context, command: CommandInteraction) {
-    match command.data.options[0].name.as_str() {
+    let subc = command.data.options[0].name.as_str();
+    let result = match subc {
         "new" => command_new(&bot, &ctx, &command).await,
         "ans" => command_ans(&bot, &ctx, &command).await,
         "giveup" => command_giveup(&bot, &ctx, &command).await,
         "help" => command_help(&bot, &ctx, &command).await,
-        _ => {}
+        _ => Err(format!("Unknown Command: {}", subc)),
     };
+
+    let msg = match result {
+        Ok(msg) => msg,
+        Err(msg) => {
+            error!(msg);
+            msg
+        }
+    };
+
+    command
+        .create_response(
+            &ctx.http,
+            CreateInteractionResponse::Message(
+                CreateInteractionResponseMessage::new().content(msg),
+            ),
+        )
+        .await
+        .unwrap();
 }
 
-async fn command_new(bot: &Bot, ctx: &Context, command: &CommandInteraction) {
+async fn command_new(
+    bot: &Bot,
+    _: &Context,
+    command: &CommandInteraction,
+) -> Result<String, String> {
     let card: serde_json::Value = serde_json::from_str(
         &reqwest::get("https://db.ygoprodeck.com/api/v7/randomcard.php")
             .await
-            .unwrap()
+            .map_err(|e| e.to_string())?
             .text()
             .await
-            .unwrap(),
+            .map_err(|e| e.to_string())?,
     )
-    .unwrap();
+    .map_err(|e| e.to_string())?;
     let card: serde_json::Value = serde_json::from_str::<serde_json::Value>(
         &reqwest::get(format!(
             "https://db.ygoprodeck.com/api/v7/cardinfo.php?misc=yes&id={}",
-            card.get("id").unwrap()
+            card.get("id").ok_or("API 応答の解析失敗")?
         ))
         .await
-        .unwrap()
+        .map_err(|e| e.to_string())?
         .text()
         .await
-        .unwrap(),
+        .map_err(|e| e.to_string())?,
     )
-    .unwrap()
+    .map_err(|e| e.to_string())?
     .get("data")
-    .unwrap()
+    .ok_or("API 応答の解析失敗")?
     .as_array()
-    .unwrap()[0]
+    .ok_or("API 応答の解析失敗")?[0]
         .clone();
-    let konami_id = card.get("misc_info").unwrap().as_array().unwrap()[0]
+    let konami_id = card
+        .get("misc_info")
+        .ok_or("API 応答の解析失敗")?
+        .as_array()
+        .ok_or("API 応答の解析失敗")?[0]
         .get("konami_id")
-        .unwrap();
+        .ok_or("API 応答の解析失敗")?;
     info!("konami_id = {}", konami_id);
     let url = format!(
         "https://www.db.yugioh-card.com/yugiohdb/card_search.action?ope=2&cid={}&request_locale=ja",
@@ -94,22 +121,34 @@ async fn command_new(bot: &Bot, ctx: &Context, command: &CommandInteraction) {
     );
 
     info!("konami_db_url = {}", url);
-    let html = reqwest::get(url).await.unwrap().text().await.unwrap();
+    let html = reqwest::get(url)
+        .await
+        .map_err(|e| e.to_string())?
+        .text()
+        .await
+        .map_err(|e| e.to_string())?;
 
     //https://github.com/causal-agent/scraper/issues/75
-    let x = || {
+    let x = || ->Result<
+      (
+        std::string::String,
+        std::string::String,
+        std::string::String,
+      ),
+      String,
+    > {
         let document = scraper::Html::parse_document(&html);
-        let name_selector = scraper::Selector::parse("#cardname h1").unwrap();
+        let name_selector = scraper::Selector::parse("#cardname h1").map_err(|e| e.to_string())?;
         let card_names = document
             .select(&name_selector)
             .next()
-            .unwrap()
+            .ok_or("遊戯王DBの解析失敗")?
             .text()
             .collect::<Vec<_>>();
         let card_name: String = card_names[2].trim().to_string();
         let card_name_ruby: String = card_names[1].trim().to_string();
-        let text_selector =
-            scraper::Selector::parse("#CardTextSet:nth-child(2) .item_box_text").unwrap();
+        let text_selector = scraper::Selector::parse("#CardTextSet:nth-child(2) .item_box_text")
+            .map_err(|e| e.to_string())?;
         let card_text: String = document
             .select(&text_selector)
             .map(|t| t.inner_html())
@@ -132,10 +171,10 @@ async fn command_new(bot: &Bot, ctx: &Context, command: &CommandInteraction) {
             .replace("<br>", "\n")
             .replace(&card_name, "<カード名>")
             .to_string();
-        (card_name, card_name_ruby, card_text)
+        Ok((card_name, card_name_ruby, card_text))
     };
 
-    let (card_name, card_name_ruby, card_text) = x();
+    let (card_name, card_name_ruby, card_text) = x()?;
 
     let content = match new_quiz(
         &bot.database,
@@ -152,11 +191,11 @@ async fn command_new(bot: &Bot, ctx: &Context, command: &CommandInteraction) {
             format!(
                 "次のカードテキストを持つ遊戯王カードは？(`/ygoquiz ans` で回答)\n\n{}\n{}",
                 card_text,
-                card.get("card_images").unwrap().as_array().unwrap()[0]
-                    .get("image_url_cropped")
-                    .unwrap()
-                    .as_str()
-                    .unwrap()
+                card.get("card_images")
+                    .and_then(|c| c
+                        .as_array()
+                        .and_then(|c| c[0].get("image_url_cropped").and_then(|c| c.as_str())))
+                    .unwrap_or("")
             )
         }
         Err(err) => {
@@ -165,18 +204,14 @@ async fn command_new(bot: &Bot, ctx: &Context, command: &CommandInteraction) {
         }
     };
 
-    command
-        .create_response(
-            &ctx.http,
-            CreateInteractionResponse::Message(
-                CreateInteractionResponseMessage::new().content(content),
-            ),
-        )
-        .await
-        .unwrap();
+    Ok(content)
 }
 
-async fn command_ans(bot: &Bot, ctx: &Context, command: &CommandInteraction) {
+async fn command_ans(
+    bot: &Bot,
+    _: &Context,
+    command: &CommandInteraction,
+) -> Result<String, String> {
     let CommandDataOptionValue::SubCommand(subopt) = &command.data.options[0].value else {
         //unreachable
         panic!()
@@ -208,18 +243,14 @@ async fn command_ans(bot: &Bot, ctx: &Context, command: &CommandInteraction) {
         ),
     };
 
-    command
-        .create_response(
-            &ctx.http,
-            CreateInteractionResponse::Message(
-                CreateInteractionResponseMessage::new().content(content),
-            ),
-        )
-        .await
-        .unwrap();
+    Ok(content)
 }
 
-async fn command_giveup(bot: &Bot, ctx: &Context, command: &CommandInteraction) {
+async fn command_giveup(
+    bot: &Bot,
+    _: &Context,
+    command: &CommandInteraction,
+) -> Result<String, String> {
     info!("Giveup: {}", command.user);
     let content = match get_quiz(&bot.database, &command.user.id.into()).await {
         Ok(quiz) => {
@@ -233,34 +264,16 @@ async fn command_giveup(bot: &Bot, ctx: &Context, command: &CommandInteraction) 
         ),
     };
 
-    command
-        .create_response(
-            &ctx.http,
-            CreateInteractionResponse::Message(
-                CreateInteractionResponseMessage::new().content(content),
-            ),
-        )
-        .await
-        .unwrap();
+    Ok(content)
 }
 
-async fn command_help(_: &Bot, ctx: &Context, command: &CommandInteraction) {
-    command
-      .create_response(
-          &ctx.http,
-          CreateInteractionResponse::Message(
-              CreateInteractionResponseMessage::new().content(
-                "Help:\n".to_owned()+
-                "遊戯王カードのカードテキストを出し、カードテキストから名前を当てる遊びです。\n"+
-                "ユーザーごとに別の問題に取り組むことができます。\n\n"+
-                "Commands:\n"+
-                "- `/ygoquiz new` - 開始\n"+
-                "- `/ygoquiz ans <card_name>` - 回答\n"+
-                "- `/ygoquiz giveup` - 問題を諦める\n"+
-                "- `/ygoquiz help` - このヘルプを表示\n"  
-              ),
-          ),
-      )
-      .await
-      .unwrap();
+async fn command_help(_: &Bot, _: &Context, _: &CommandInteraction) -> Result<String, String> {
+    Ok("Help:\n".to_owned()
+        + "遊戯王カードのカードテキストを出し、カードテキストから名前を当てる遊びです。\n"
+        + "ユーザーごとに別の問題に取り組むことができます。\n\n"
+        + "Commands:\n"
+        + "- `/ygoquiz new` - 開始\n"
+        + "- `/ygoquiz ans <card_name>` - 回答\n"
+        + "- `/ygoquiz giveup` - 問題を諦める\n"
+        + "- `/ygoquiz help` - このヘルプを表示\n")
 }
